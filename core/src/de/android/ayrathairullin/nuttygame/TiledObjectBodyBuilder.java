c8f7bcf275ad655e0e1f4f8bda5946f23f7a22f0
Ayrat <ayrat1@mail.ru>
2018-04-25 08:35:01 +0300

    210

---
+++ core/src/de/android/ayrathairullin/nuttygame/TiledObjectBodyBuilder.java
@@ -0,0 +1,15 @@
+package de.android.ayrathairullin.nuttygame;
+
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class TiledObjectBodyBuilder {
+    private static final float PIXELS_PER_TILE = 32;
+    private static final float HALF = .5f;
+
+    public static void buildBuildingBodies(TiledMap tiledMap, World world) {
+
+
+    }
+}
